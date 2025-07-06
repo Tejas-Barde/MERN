1b7795de033ffe1b9d9060ef74abd01257d37d2f
Tejas-Barde <tjbarde30@gmail.com>
2025-07-06 23:33:31 +0530

    Python

---
+++ Python/Day1/p00.py
@@ -0,0 +1,4 @@
+from sample import get_sum,print_name 
+
+print(get_sum(100,200))
+print_name()
---
+++ Python/Day1/sample.py
@@ -0,0 +1,10 @@
+def get_sum(a,b):
+  return a+b
+
+def print_name():
+  print(__name__)
+
+print("In Sample File")
+
+if __name__ == "__main__":
+  print(get_sum(10,20))
